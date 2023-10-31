233f1c59a0a327c5c0081afc87d3a1d02e68bc15
Jay Jansheski <jay@jansheski.com>
2023-10-31 14:21:52 -0700

    Commit

---
+++ lib/active_shipping/version.rb
@@ -0,0 +1,3 @@
+module ActiveShipping
+  VERSION = "0.11.2"
+end
---
+++ lib/vendor/test_helper.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+$:.unshift(File.dirname(__FILE__) + '/../lib')
+
+
+require 'test/unit'
+require 'active_shipping'
+require 'mocha'
